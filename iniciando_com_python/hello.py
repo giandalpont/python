7d78b724ecd54772e7b12ab1b8db000b21ac90ff
Gian Carlos Dal Pont <giandalpont@MacBook-Pro-de-Gian.local>
2019-12-03 00:10:21 -0200

    Brincando com listas

--- iniciando_com_python/hello.py
+++ iniciando_com_python/hello.py
@@ -1,7 +1,39 @@
 def hello():
     return "Hello world"
+
+
 print(hello())
+
 
 def sam(e, r):
     return e + r
+
+
 print(sam(3, 6))
+
+number = [1, 1, 2, 3, 4]
+number2 = [23, 23, 12, 112, 9]
+
+print(number)
+print(number2)
+num = number + number2
+print(num)
+
+print(number[1])
+print(number[2])
+print(number[:3])
+
+mix = [1, 2, '3', [1, 2, 3]]
+print(mix)
+print(mix[3][2])
+
+
+numbers = [0, 1, 2, 4]
+print(numbers)
+numbers.append(8)  # add
+numbers.append(8)  # add
+print(numbers)
+numbers.remove(8)  # remove remove somente o primeiro
+print(numbers)
+del(numbers[4]) # deletando pelo índice
+print(numbers)
