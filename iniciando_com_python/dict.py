06b2b7c88ffdf1422cd1eaa315364391648897be
Gian Carlos Dal Pont <giandalpont@MacBook-Pro-de-Gian.local>
2019-12-03 20:56:56 -0200

    Trabalhando com dicionários

---
+++ iniciando_com_python/dict.py
@@ -0,0 +1,23 @@
+cars = {}
+print(cars)
+
+cars['corola'] = "red"
+cars['up'] = "green"
+cars['320'] = "black"
+
+print("\ncars")
+print(cars)
+
+print("\nkey")
+print(cars.keys())
+
+print("\nValues")
+print(cars.values())
+
+people = dict(Wesley='Father', Maria='Mother', Sarah='baby')
+print('\npeople')
+print(people)
+
+if 'Wesley' in people:
+    print("\npeople['Wesley']")
+    print(people['Wesley'])
