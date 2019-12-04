5a0eb3f8f0c44170504339a32abfabc40cfe5910
Gian Carlos Dal Pont <giandalpont@MacBook-Pro-de-Gian.local>
2019-12-03 23:42:11 -0200

    Criando nossa classe

---
+++ python_OO/car.py
@@ -0,0 +1,7 @@
+class Car:
+
+    def __init__(self):
+        self.name = "320"
+        self.meka = 'BMW'
+        self.year = '2020'
+        self.color = 'silver'
--- python_OO/index.py
+++ python_OO/index.py
@@ -1,13 +1,22 @@
-from control.math.cal import calculate, calculate_sub
-from control.string.str import hello
+# from control.math.cal import calculate, calculate_sub
+# from control.string.str import hello
+from car import Car
 
-print("cal")
-calculate(2, 4)
-print("\nSub")
-calculate_sub(10, 4)
+# print("cal")
+# calculate(2, 4)
+# print("\nSub")
+# calculate_sub(10, 4)
 
-print("\ncalculate()")
-calculate(2, 4)
+# print("\ncalculate()")
+# calculate(2, 4)
 
-print("\n")
-print(hello())
+# print("\n")
+# print(hello())
+
+
+car = Car()
+print(car)
+print(car.name)
+print(car.meka)
+print(car.color)
+print(car.year)
