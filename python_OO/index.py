bd561ba259c79d6489f4669f766bae8011c1cf00
Gian Carlos Dal Pont <giandalpont@MacBook-Pro-de-Gian.local>
2019-12-03 22:55:46 -0200

    Criando nosso primeiro Package

---
+++ python_OO/index.py
@@ -0,0 +1,13 @@
+from control.math.cal import calculate, calculate_sub
+from control.string.str import hello
+
+print("cal")
+calculate(2, 4)
+print("\nSub")
+calculate_sub(10, 4)
+
+print("\ncalculate()")
+calculate(2, 4)
+
+print("\n")
+print(hello())
