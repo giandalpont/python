f177765f2f21b46c497617789804c04a870485d8
giandalpont <giandalpont@gmail.com>
2020-07-03 01:43:53 -0300

    Autenticidade da conta bancária

--- caixa_eletronico/main.py
+++ caixa_eletronico/main.py
@@ -11,6 +11,28 @@ import getpass
 account_typed = input('Digite sua conta: ')
 password_typed = getpass.getpass('Digite sua senha: ')
 
-print(account_typed)
-print(password_typed)
+## print(account_typed)
+## print(password_typed)
+
+accounts_list = {
+    '0001-02': {
+        'agency': '0001-02',
+        'password': '01',
+        'name': 'João da Silva',
+        'value': 0,
+    },
+    '0002-02': {
+        'agency': '0002-02',
+        'password': '02',
+        'name': 'Maria da Silva',
+        'value': 0,
+    },
+}
+
+if account_typed in accounts_list and password_typed == accounts_list[account_typed]['password']:
+    print('Conta valida')
+else:
+    print('Conta inválida')
+
+## agência, senha, nome, valor
 
