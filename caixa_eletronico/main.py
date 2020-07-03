b4501af6dd4dc821a70048544ab7206652e3013f
giandalpont <giandalpont@gmail.com>
2020-07-03 01:58:38 -0300

    Mostrar o saldo do correntista

--- caixa_eletronico/main.py
+++ caixa_eletronico/main.py
@@ -7,6 +7,8 @@ print("*******************************************")
 print(' ')
 
 import getpass
+import os
+clear = lambda: os.system('clear')
 
 account_typed = input('Digite sua conta: ')
 password_typed = getpass.getpass('Digite sua senha: ')
@@ -19,18 +21,28 @@ accounts_list = {
         'agency': '0001-02',
         'password': '01',
         'name': 'João da Silva',
-        'value': 0,
+        'value': 100,
     },
     '0002-02': {
         'agency': '0002-02',
         'password': '02',
         'name': 'Maria da Silva',
-        'value': 0,
+        'value': 50,
     },
 }
 
+clear()
+
 if account_typed in accounts_list and password_typed == accounts_list[account_typed]['password']:
     print('Conta valida')
+    print(' ')
+    
+    print('1 - Saldo')
+    option_typed = input('Escolha uma das opções acima: ')
+    if option_typed == '1':
+        print(' ')
+        print('Seu saldo é %s' % accounts_list[account_typed]['value'])
+
 else:
     print('Conta inválida')
 
