e83f33c8fa6d20b1e631eeda0b7df6f1ab058c8b
giandalpont <giandalpont@gmail.com>
2020-07-03 14:44:20 -0300

    Inclusão de cédulas no caixa

--- caixa_eletronico/main.py
+++ caixa_eletronico/main.py
@@ -25,13 +25,27 @@ while True:
             'password': '01',
             'name': 'João da Silva',
             'value': 100,
+            'admin': False,
         },
         '0002-02': {
             'agency': '0002-02',
             'password': '02',
             'name': 'Maria da Silva',
             'value': 50,
+            'admin': False,
         },
+        '1111-11': {
+            'password': '1111',
+            'name': 'Gerente',
+            'value': 0,
+            'admin': True,
+        }
+    }
+
+    money_slips = {
+        '20': 5,
+        '50': 5,
+        '100': 5,
     }
 
     clear()
@@ -40,11 +54,23 @@ while True:
         print('Olá, %s' % accounts_list[account_typed]['name'] + '!')
         print(' ')
         
+        if  accounts_list[account_typed]['admin']:
+            print('0 - Incluir cédulas')
+        
         print('1 - Saldo')
         option_typed = input('Escolha uma das opções acima: ')
+
         if option_typed == '1':
             print(' ')
             print('Seu saldo é %s' % accounts_list[account_typed]['value'])
+
+        if option_typed == '0' and accounts_list[account_typed]['admin']:
+            amount_typed =  input('Digite a quantidade de cédulas: ')
+            money_bill_typed = input('Digite a cédula a ser incluída: ')
+
+            money_slips[money_bill_typed] += int(amount_typed)
+            
+            print(money_slips)
 
     else:
         print('Conta inválida')
