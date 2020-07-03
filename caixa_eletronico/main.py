7fbef3c3c00a1caef7d2eb8ba49b0bd383af6eab
giandalpont <giandalpont@gmail.com>
2020-07-03 01:13:35 -0300

    Capturar dados do teclado

--- caixa_eletronico/main.py
+++ caixa_eletronico/main.py
@@ -6,3 +6,11 @@ print("*********** Caixa Eletrônico  *************")
 print("*******************************************")
 print(' ')
 
+import getpass
+
+account_typed = input('Digite sua conta: ')
+password_typed = getpass.getpass('Digite sua senha: ')
+
+print(account_typed)
+print(password_typed)
+
